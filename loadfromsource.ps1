f778562bea7dbf1bfb082b23644189d0bfb5a479
Aleksandr Serdiukov <ntwwwnt@gmail.com>
2022-10-13 17:25:30 +0300

    Load from source fixed

--- loadfromsource.ps1
+++ loadfromsource.ps1
@@ -1,5 +1,5 @@
 #!/bin/bash
-Set-Variable "HICT_DIR" "${PSScriptRoot}/../hict/"
+Set-Variable "HICT_DIR" "${PSScriptRoot}/../HiCT_Library/"
 $env:PYTHONPATH += ";${HICT_DIR}"
 echo "Setting HICT_DIR = ${HICT_DIR} and PYTHONPATH = ${env:PYTHONPATH}"
 python -m hict_server
--- loadfromsource.sh
+++ loadfromsource.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-HICT_DIR="$SCRIPT_DIR/../hict/"
+HICT_DIR="$SCRIPT_DIR/../HiCT_Library/"
 export PYTHONPATH="$PYTHONPATH:$HICT_DIR"
 python3 -m hict_server
