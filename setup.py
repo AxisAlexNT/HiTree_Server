8be0f8bfee1aa83a4113a39c695bd827e35ccdb7
Aleksandr Serdiukov <ntwwwnt@gmail.com>
2023-05-11 18:34:24 +0300

    Updated versioning and requirements

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", mode="rt", encoding="utf-8") as f:
 
 setup(
     name='hict_server',
-    version='0.1.1rc1',
+    version='0.1.3rc1',
     packages=list(set(['hict_server', 'hict_server.api_controller',
               'hict_server.api_controller.dto']).union(find_packages())),
     url='https://genome.ifmo.ru',
@@ -17,7 +17,8 @@ setup(
     author_email='',
     description='Development version of API and tiling server for HiCT interactive Hi-C scaffolding tool.',
     install_requires=list(set([
-        'hict>=0.1.1rc1,<1.0',
+        'hict>=0.1.3rc1,<1.0',
+        'hict_utils>=0.1.3rc1,<1.0',
     ]).union(requirements)),
     entry_points={
         'console_scripts': ['hict_server=hict_server.api_controller.dev_demo_server:main'],
