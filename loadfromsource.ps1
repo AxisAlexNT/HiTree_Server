2832820ef94e47eb766fa54c54d3d20ec17b70dc
Aleksandr Serdiukov <ntwwwnt@gmail.com>
2024-06-27 03:15:56 +0400

    Header added from GitHub

--- loadfromsource.ps1
+++ loadfromsource.ps1
@@ -1,4 +1,5 @@
 Set-Variable "HICT_DIR" "${PSScriptRoot}/../HiCT_Library/"
-$env:PYTHONPATH += ";${HICT_DIR}"
+Set-Variable "HICT_UTILS" "${PSScriptRoot}/../HiCT_Utils/"
+$env:PYTHONPATH += ";${HICT_DIR};${HICT_UTILS}"
 echo "Setting HICT_DIR = ${HICT_DIR} and PYTHONPATH = ${env:PYTHONPATH}"
 python -m hict_server
